28024c19096746bfc3985dff1237f0a7c63448e2
Marcio Rodrigues <luiz.rodrigues@gmail.com>
2022-03-01 14:14:01 -0300

    add themes

---
+++ src/themes/index.ts
@@ -0,0 +1,10 @@
+export default {
+  color: {
+    primary_red: '#F2264B',
+    primary_black: '#000',
+    primary_dark: '#313140',
+    primary_grey: '#b7b7c8',
+    primary_silver: '#F8F8F8',
+    primary_white: '#FFFFFF'
+  }
+}
