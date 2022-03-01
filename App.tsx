28a443e64425d4926852bca77947d71b811c802d
Marcio Rodrigues <luiz.rodrigues@gmail.com>
2022-03-01 14:31:36 -0300

    config theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/Home';
 
+import { ThemeProvider} from 'styled-components/native'
+import theme from './src/themes';
+import AppLoading from 'expo-app-loading';
+import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -6,5 +6,11 @@ export default {
     primary_grey: '#b7b7c8',
     primary_silver: '#F8F8F8',
     primary_white: '#FFFFFF'
+  },
+
+  fonts: {
+    regular: 'Roboto_400Regular',
+    medium: 'Roboto_500Medium',
+    bold: 'Roboto_700Bold'
   }
 }
