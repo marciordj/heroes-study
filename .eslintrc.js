d8ec116abd3041dd186417239646fa95d6d91478
Marcio Rodrigues <luiz.rodrigues@gmail.com>
2022-03-01 14:13:27 -0300

    add themes, fonts and styled components

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const generator = require('@eduzz/eslint-config-houston/configs/generator');
+
+module.exports = {
+  extends: ['@eduzz/eslint-config-houston/native'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'max-lines': ['error', 400],
+    'padding-line-between-statements': [
+     'error',
+     { blankLine: 'always', prev: ['block-like', 'function'], next: '*' },
+     { blankLine: 'always', prev: ['*'], next: ['block-like', 'function'] },
+     { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+     { blankLine: 'any', prev: ['export', 'import'], next: ['export', 'import'] },
+     { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['export'] }
+    ]
+  }
+};
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>Hello world</Title>
+    </Container>
+  )
+}
+
+
+export default Home;
